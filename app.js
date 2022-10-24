2656a111a38219e5a88f209e8381140364069070
Ram Narayan Yadav <yram08913@gmail.com>
2022-10-25 01:28:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// ---------------------------------------------
+// 02_basics
+// helper methods
+// document.write("hello world");
+// alert("hello world");
+// console.log("helllo world");
+
+// document.write("hello world");
+// alert("hello world");
+// console.log("hello world");
+
+// document.write({ name: "ram" });
+// alert({ name: "ram" });
+// console.log({ name: "ram" });
+// --------------------------------------------
+// 03_statements and comments
+// I woke up early him today
+// statements - sets of instructions
+// comments - ctrl+/
+// This is console statement
+// console.log("Hello world");
+// console.log("Hello people");
+// console.log("Hello pidoori");
+// console.log("Hello Mommy");
+// console.log("Hello chetii");
+// console.log("Hello Lailuuu");
+// console.log("Hello Lala");
+// console.log("Hello pita ji");
+// document.write("hello world");
+// ------------------------------------------------
+// 04_variables
+// variables- most basic building block
+// variables- stores, access , modify === value
+// Declare , assignment operator, assign value
+// assign variable value later , modify existing
+// let Name = "Ram Narayan Yadav Noida Up Uk";
+// let address, zip, state;
+// address = "101 main street";
+// zip = "201304";
+// state = "up";
+// Name = "pidoori";
+// console.log(address, zip, state);
+// console.log(Name);
